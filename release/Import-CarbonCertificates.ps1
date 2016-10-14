a8cdfd5637f05e513928aed78d83cfadc8e61fad
Dennis Frostlander <dennis.frostlander@gmail.com>
2016-10-14 23:38:23 +0200

    deployment

--- release/Import-CarbonCertificates.ps1
+++ release/Import-CarbonCertificates.ps1
@@ -9,13 +9,7 @@
     [string] $pwd_encrypt,
     
     [Parameter(Mandatory=$true)]
-    [string] $pwd_cluster_qa2,
-    
-    [Parameter(Mandatory=$true)]
-    [string] $pwd_idsrv_release,
-    
-    [Parameter(Mandatory=$true)]
-    [string] $pwd_ssl_debug
+    [string] $pwd_cluster_qa2
 )
 
 function Import($path, $password)
@@ -28,6 +22,3 @@ Import .\carbon-secrets\azure\subscription-qa1.pfx $pwd_subscription_qa1
 Import .\carbon-secrets\azure\subscription-qa2.pfx $pwd_subscription_qa2
 Import .\carbon-secrets\azure\encrypt.pfx $pwd_encrypt
 Import .\carbon-secrets\azure\cluster-qa2.pfx $pwd_cluster_qa2
-Import .\carbon-secrets\azure\idsrv-release.pfx $pwd_idsrv_release
-
-Import .\carbon-secrets\ssl-debug.pfx $pwd_ssl_debug
