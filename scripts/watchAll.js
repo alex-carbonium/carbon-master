22289353ae7ed2cd303fec1f2885a47e2fbd7958
85frost <dennis.kukurudza@gmail.com>
2017-03-30 00:07:24 +0200

    d.ts watcher

--- scripts/watchAll.js
+++ scripts/watchAll.js
@@ -1,8 +1,12 @@
 var cp = require("child_process");
 var path = require("path");
+var fs = require("fs");
 
-spawnAndPipe("core: ", "npm.cmd", ["start"], fullPath("../carbon-core"));
-spawnAndPipe("ui: ", "npm.cmd", ["start", "--", "--linkCore"], fullPath("../carbon-ui"));
+var npm = process.platform === "win32" ? "npm.cmd" : "npm";
+
+spawnAndPipe("core: ", npm, ["start"], fullPath("../carbon-core"));
+spawnAndPipe("ui: ", npm, ["start", "--", "--linkCore"], fullPath("../carbon-ui"));
+watchFolder(fullPath("../carbon-core/mylibs/definitions"), fullPath("../carbon-ui/target"), /carbon\-.*ts$/gi);
 
 function spawnAndPipe(prefix, program, args, cwd){
     var childProcess = cp.spawn(program, args, {
@@ -16,6 +20,28 @@ function spawnAndPipe(prefix, program, args, cwd){
     childProcess.stderr.on("data", function(data){
         process.stderr.write(prefix + data.toString());
     });
+}
+
+function watchFolder(source, target, filePattern){
+    var files = fs.readdirSync(source).filter(x => {
+        filePattern.lastIndex = 0;
+        return filePattern.test(x)
+    });
+
+    for (var i = 0; i < files.length; ++i){
+        copyFile(path.join(source, files[i]), path.join(target, files[i]));
+    }
+
+    fs.watch(source, (e, filename) => {
+        if (filePattern.test(filename)){
+            copyFile(path.join(source, filename), path.join(target, filename));
+        }
+    });
+}
+
+function copyFile(source, target){
+    console.log("Copying", path.basename(source), "to", target);
+    fs.createReadStream(source).pipe(fs.createWriteStream(target));
 }
 
 function fullPath(relativePath){
