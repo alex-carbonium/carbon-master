6985d787d9762a6adb661e3d4d296bab8c7e87cd
Dennis Frostlander <dennis.kukurudza@gmail.com>
2018-11-25 20:56:56 +0000

    Updated Set-CarbonDevPort.ps1

--- release/Set-CarbonDevPort.ps1
+++ release/Set-CarbonDevPort.ps1
@@ -7,7 +7,7 @@ $xml = [xml](Get-Content $serviceManifest)
 $endpoint = $xml.ServiceManifest.Resources.Endpoints.ChildNodes | where {$_.Name -eq "ServiceEndpoint"}
 $endpoint.Port = "80"
 
-$endpoint = $xml.ServiceManifest.Resources.Endpoints.ChildNodes | where {$_.Name -eq "SslServiceEndpoint"}
-$endpoint.Port = "443"
+#$endpoint = $xml.ServiceManifest.Resources.Endpoints.ChildNodes | where {$_.Name -eq "SslServiceEndpoint"}
+#$endpoint.Port = "443"
     
 $xml.Save($serviceManifest)
